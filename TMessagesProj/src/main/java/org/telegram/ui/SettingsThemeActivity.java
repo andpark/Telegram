363e573b220f92679b573fab1c56644361e7547d
SooHwan Park <zilitwo@gmail.com>
2014-10-22 17:10:00 +0900

    테마설정창 다시

--- TMessagesProj/src/main/java/org/telegram/ui/SettingsThemeActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsThemeActivity.java
@@ -1,5 +1,7 @@
 package org.telegram.ui;
 
+import org.telegram.android.LocaleController;
+import org.telegram.ui.Views.ActionBar.ActionBarLayer;
 import org.telegram.ui.Views.ActionBar.BaseFragment;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,18 @@ public class SettingsThemeActivity extends BaseFragment {
     @Override
     public View createView(LayoutInflater inflater, ViewGroup container) {
         if (fragmentView == null) {
+            actionBarLayer.setDisplayHomeAsUpEnabled(true, R.drawable.ic_ab_back);
+            actionBarLayer.setBackOverlay(R.layout.updating_state_layout);
+            actionBarLayer.setTitle(LocaleController.getString("SelectTheme", R.string.SelectTheme));
+            actionBarLayer.setActionBarMenuOnItemClick(new ActionBarLayer.ActionBarMenuOnItemClick() {
+                @Override
+                public void onItemClick(int id) {
+                    if (id == -1) {
+                        finishFragment();
+                    }
+                }
+            });
+
             fragmentView = inflater.inflate(R.layout.settings_theme_layout, container, false);
         } else {
             ViewGroup parent = (ViewGroup)fragmentView.getParent();
